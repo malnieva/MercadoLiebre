fd77ffa15b72f4466a46bf01aa741f2dfc734f0a
malnieva <malnieva@gmai.com>
2023-10-31 17:05:59 -0300

    agregando cambios en home, login, register y styles

--- app.js
+++ app.js
@@ -1,15 +1,26 @@
-const express = require('express');
-const path = require('path');
+const express = require("express");
+const path = require("path");
 
 const app = express();
 
-const publicPath = path.join(__dirname, 'public');
-
+const publicPath = path.join(__dirname, "public");
 app.use(express.static(publicPath));
 
-const port = 3030
-app.listen(port,()=> console.log(`http://localhost:${port}`));
+const port = 3030;
+app.listen(port, () => console.log(`http://localhost:${port}`));
 
-app.get('/', (req,res) => {
-    res.sendFile(path.join(__dirname, "views", "home.html"));
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "views", "home.html"));
+});
+
+app.get("/registro", (req, res) => {
+  res.sendFile(path.join(__dirname, "views", "form_pages", "register.html"));
+});
+
+app.get("/login", (req, res) => {
+  res.sendFile(path.join(__dirname, "views", "form_pages", "login.html"));
+});
+
+app.use(function (req, res, next) {
+  res.status(404).send("¡Lo siento, no puedo encontrar eso!");
 });
